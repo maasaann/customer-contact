75d0b3dd7c654a3b15c81a9208f8ce394eaec942
maasaann <meyutana@gmail.com>
2025-05-27 12:57:18 +0900

    2025-05-27 12:57

--- initialize.py
+++ initialize.py
@@ -14,12 +14,12 @@ import streamlit as st
 import tiktoken
 from langchain_openai import ChatOpenAI
 from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
-from langchain import SerpAPIWrapper
+#from langchain import SerpAPIWrapper
+from langchain_community.utilities import SerpAPIWrapper
 from langchain.tools import Tool
 from langchain.agents import AgentType, initialize_agent
 import utils
 import constants as ct
-
 
 
 ############################################################
